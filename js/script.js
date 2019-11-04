8008f27b77b192b634202c555f9b99fc83cf08d4
Maciej Krzyżowski <maciek.krzyzowski44@gmail.com>
2019-11-04 16:01:40 +0100

    Add function titleClickHandler

--- js/script.js
+++ js/script.js
@@ -1,31 +1,33 @@
 'use strict';
 
 function titleClickHandler(event) {
-    console.log('Link was clicked!');
-    console.log(event)
-        /* remove class 'active' from all article links  */
-    const activeLinks = document.querySelectorAll('.titles a.active');
 
+    const clickedElement = this;
+    event.preventDefault();
+    console.log('Link was clicked!');
+    console.log(event);
+    /* remove class 'active' from all article links  */
+    const activeLinks = document.querySelectorAll('.titles a.active');
     for (let activeLink of activeLinks) {
         activeLink.classList.remove('active');
     }
-
+    /* remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts .active');
-
     for (let activeArticle of activeArticles) {
         activeArticle.classList.remove('active');
     }
     /* add class 'active' to the clicked link */
-
-    /* remove class 'active' from all articles */
-
+    this.classList.add('active');
+    console.log('clickedElement:', clickedElement);
     /* get 'href' attribute from the clicked link */
-
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log(articleSelector);
     /* find the correct article using the selector (value of 'href' attribute) */
-
+    const tergetArticle = document.querySelector(articleSelector);
+    console.log(tergetArticle);
     /* add class 'active' to the correct article */
+    tergetArticle.classList.add('active');
 }
-
 const links = document.querySelectorAll('.titles a');
 
 for (let link of links) {
